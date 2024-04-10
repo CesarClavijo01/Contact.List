094db60e78fa08e6b1719819d3df70a62ff55f84
CesarClavijo01 <cesarclavijo778@gmail.com>
2024-04-09 20:55:48 -0500

    functional commit

--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -17,39 +17,47 @@ useEffect( () => {
         }
     
     }
-    fetchSelectedContact()
-    console.log(contact)
-}, [setSelectedId])
 
-// return(
-// <table>
-//     <thead>
-//         <tr>
-//             <th colSpan={2}></th>
-//         </tr>
-//     </thead>
-//     <tbody>
-//         <tr>
-//             <th>Phone Number</th>
-//             <td>{contact.phone}</td>
-//         </tr>
-//         <tr>
-//             <th>Email</th>
-//             <td>{contact.email}</td>
-//         </tr>
-//         <tr>
-//             <th>Address</th>
-//             <td>{contact.address}</td>
-//         </tr>
-//         <tr>
-//             <th>Company</th>
-//             <td>{contact.company.name}</td>
-//         </tr>
-//         <tr>
-//             <th>Web Site</th>
-//             <td>{contact.website}</td>
-//         </tr>
-//     </tbody>
-// </table>
-// )
+    fetchSelectedContact()
+}, [selectedId])
+
+if(contact == null){
+    return(
+        <div>Loading</div>
+    )
+}
+
+return(
+<>
+    <table>
+        <thead>
+            <tr>
+                <th colSpan={2}>{contact.name}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>Phone Number</th>
+                <td>{contact.phone}</td>
+            </tr>
+            <tr>
+                <th>Email</th>
+                <td>{contact.email}</td>
+            </tr>
+            <tr>
+                <th>Address</th>
+                <td>{contact.address.street}</td>
+            </tr>
+            <tr>
+                <th>Company</th>
+                <td>{contact.company.name}</td>
+            </tr>
+            <tr>
+                <th>Web Site</th>
+                <td>{contact.website}</td>
+            </tr>
+        </tbody>
+    </table>
+</>
+)
 }
